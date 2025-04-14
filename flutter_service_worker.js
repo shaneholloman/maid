657401fa0a61de328ac727c9f2cf60cb21cf515b
danemadsen <dane_madsen@hotmail.com>
2025-04-14 23:49:08 +0000

    Deploying to gh-pages from @ Mobile-Artificial-Intelligence/maid@2fa90b13520166716d1f6f32b404717c699eaea3 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -29,14 +29,14 @@ const RESOURCES = {"canvaskit/skwasm.js": "f2ad9363618c5f62e813740099a80e63",
 "assets/AssetManifest.json": "d8fe15e720999909b84dbffb0e2b4ee0",
 "version.json": "d3096168bbec7ccd1478e044c0360e0a",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"main.dart.js": "cd6fefc930057d6db6ca54a3af488fa7",
+"main.dart.js": "4f48180f71464f8e9e81f7a59e9edbe3",
 "icons/Icon-maskable-512.png": "3248a332d073174db5eede30c2462001",
 "icons/Icon-192.png": "c06e777073eb0bcfb3b33153b35e1c1e",
 "icons/Icon-512.png": "3248a332d073174db5eede30c2462001",
 "icons/Icon-maskable-192.png": "ff973b828abd069edc5e43b1c0525485",
 "manifest.json": "3a3018bd126e2670ee844d20ebdf10f3",
 "favicon.png": "93beecd2834ac8b449d534f903a61a84",
-"flutter_bootstrap.js": "26dc24f8fb16aae713d2895cd05678b6"};
+"flutter_bootstrap.js": "83e54e9a6360491e251df463bde748c9"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
