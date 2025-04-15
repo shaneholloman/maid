9c30c0aeb433647d185eca13b51a6c52fefb4afd
danemadsen <dane_madsen@hotmail.com>
2025-04-15 02:54:58 +0000

    Deploying to gh-pages from @ Mobile-Artificial-Intelligence/maid@2a71c7848c3a4ba9fdeae538faa978078b7311de 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4271994472"
+    serviceWorkerVersion: "3486986579"
   }
 });
