0db7ed45c24675488d2f6aa5fe19571d5b08a37c
danemadsen <danemadsen@users.noreply.github.com>
2025-04-16 12:07:24 +0000

    Deploying to gh-pages from @ Mobile-Artificial-Intelligence/maid@33d87c23bf2f19250bc6a5650973374e4067259c 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -36,7 +36,7 @@ const RESOURCES = {"canvaskit/skwasm.js": "f2ad9363618c5f62e813740099a80e63",
 "icons/Icon-maskable-192.png": "ff973b828abd069edc5e43b1c0525485",
 "manifest.json": "3a3018bd126e2670ee844d20ebdf10f3",
 "favicon.png": "93beecd2834ac8b449d534f903a61a84",
-"flutter_bootstrap.js": "71ed6cbd1184d431934796d59f36b2c8"};
+"flutter_bootstrap.js": "7ee66ad69ca501af11efa7749ada80cb"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
